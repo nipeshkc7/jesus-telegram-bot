a50f6939e89fa1d48890530887ca4846d1fc6807
Arpan KC <nipeshkc7@gmail.com>
2022-01-20 09:22:39 +1100

    Update function to log body

--- Jesus/index.js
+++ Jesus/index.js
@@ -1,5 +1,7 @@
 exports.handler = (event, context, callback) => {
 
+    console.log(event);
+
     const response = {
         statusCode: 200,
         body: JSON.stringify('This is a test Lambda function deployed using SAM Template!')
