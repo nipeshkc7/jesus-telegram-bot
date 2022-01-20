a3d18ce52d49bf8a6b2ec7cd04012c0a8aef378b
Arpan KC <nipeshkc7@gmail.com>
2022-01-20 20:53:53 +1100

    respond to messages

--- Jesus/index.js
+++ Jesus/index.js
@@ -1,13 +1,50 @@
-exports.handler = (event, context, callback) => {
+const axios = require('axios');
 
-    console.log(event.body);
+exports.handler = async (event, context, callback) => {
+
+    const body = JSON.parse(event.body);
+    const reply = processMessage(body.message);
+
+    try {
+        await axios.get(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage?chat_id=${body.message.chat.id}&text=${encodeURI(reply.text)}`)
+        const gif = await axios.get(`https://g.tenor.com/v1/random?key=${process.env.TENOR_KEY}&q=${encodeURI(reply.gif)}&limit=1`);
+        await axios.get(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendAnimation?chat_id=${body.message.chat.id}&animation=${encodeURI(gif.data.results[0].media[0].gif.url)}`)
+    } catch (e) {
+        console.error('Error sending message', e);
+    }
 
     const response = {
         statusCode: 200,
-        body: JSON.stringify('This is a test Lambda function deployed using SAM Template!')
-
+        body: JSON.stringify('OK')
     };
 
     callback(null, response);
 
 };
+
+function processMessage(message) {
+    // get amount
+    switch (message.text.toLowerCase()) {
+        case 'oi':
+            return {
+                text: `aah k vo ${message.from.first_name}`,
+                gif: "what's up"
+            };
+        case 'k cha':
+            return {
+                text: `aah thikai cha ${message.from.first_name}, timro k cha`,
+                gif: "how are you doing"
+            };
+        case 'thikai cha':
+            return {
+                text: `lala`,
+                gif: "whatever"
+            };
+        default:
+            return {
+                text: `k vanya bujina`,
+                gif: "confused"
+            };
+    }
+}
+
