0036f45e7473659c4ae5e432a58e1efe6c6c9924
Arpan KC <nipeshkc7@gmail.com>
2022-01-25 17:02:20 +1100

    more tests

--- __tests__/processMessage.test.js
+++ __tests__/processMessage.test.js
@@ -1,8 +1,11 @@
 const { processMessage } = require('../Jesus/processMessage');
 
+let people;
+
 describe('Testing message processing', () => {
-    it('should divide bills equally among people', () => {
-        const people = {
+
+    beforeEach(()=>{
+        people = {
             'John': {
                 'spent': 0,
                 'owes': {
@@ -24,15 +27,44 @@ describe('Testing message processing', () => {
                     'Mary': 0,
                 }
             },
-        };
+        }; 
+    })
+    it('should divide bills equally among people', () => {
+
         const message = {
             user: 'John',
             text: 'jesus i spent 300 dollars on food'
         }
 
         const result = processMessage(message, people);
-        expect(result).toEqual('');
+        expect(result.peopleRecord.John.spent).toEqual(300);
+        expect(result.peopleRecord.Mary.owes.John).toEqual(100);
+        expect(result.peopleRecord.Bob.owes.John).toEqual(100);
     });
+
+    test.each([['John', 300, 100], ['Mary', 600, 200], ['Bob', 100, 33.33]])(
+        "should divide bills equally for %s",(user, spent, divided) => {
+               const result = processMessage({
+                   user,
+                   text: `jesus i spent ${spent} dollars on food`
+               }, people);
+
+               Object.keys(result.peopleRecord).forEach(entry => {
+                   if(entry !== user){
+                       expect(result.peopleRecord[entry].owes[user]).toEqual(divided);
+                   }
+               })
+            });
+
+     test.each(['John', 'Mary', 'Bob'])(
+         "should clear bills for %s",(user) => {
+                const result = processMessage({
+                    user,
+                    text: 'jesus i cleared my bills'
+                }, people);
+                expect(result.peopleRecord[user].owes).toEqual({});
+         }
+     )
 
     it('should throw error if no user', () => {
         expect(() => {
