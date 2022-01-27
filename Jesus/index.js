5889b12413a7b23327a86d6322fcee764288a570
Arpan KC <nipeshkc7@gmail.com>
2022-01-28 09:14:13 +1100

    adding message sender to the group

--- Jesus/index.js
+++ Jesus/index.js
@@ -2,87 +2,38 @@ const axios = require('axios');
 const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient();
 const tableName = process.env.TABLE_NAME;
-
-async function createItem(item) {
-    // const params = {
-    //     TableName: 'People',
-    //     Item: {
-    //         id: '12345',
-    //         price: 100.00
-    //     }
-    // }
-
-    try {
-        await docClient.put({
-            TableName: tableName,
-            Item: item
-        }).promise();
-
-    } catch (err) {
-        console.error(`Error Creating Item ${err.message}`);
-        console.log(err);
-        return err;
-    }
-}
+const { processMessage } = require('./processMessage');
 
 async function getItem(id) {
-    // const params = {
-    //     TableName: 'People',
-    //     Key: {
-    //         id: '1'
-    //     }
-    // };
-
     const data = await docClient.get({
         TableName: tableName,
         Key: {
-            id: id 
+            id: id
         }
     }).promise();
 
     return data.Item;
 }
 
-// async function updateItem(id, item) {
-//     const data = await docClient.update({
-//         TableName: tableName,
-//         Key: {
-//             id: id
-//         },
-//         UpdateExpression: 'set message = :r',
-//         ExpressionAttributeValues: {
-//             ':r': item
-//         },
-//         ReturnValues: 'UPDATED_NEW'
-//     }).promise();
-
-//     return data;
-// }
-
-// async function updateItem(id, item){
-//     const data = await docClient.put({
-//         TableName: tableName,
-//         Item: {
-//             ...item
-//         }
-
-//     })
-// }
-
-async function update(id, item){
+async function update(id, item) {
     await docClient.put({
         TableName: tableName,
         Item: {
             ...item,
             id: id,
         }
-    }).promise(); 
+    }).promise();
 }
 
-async function addMembers(chatId, newChatMembers){
-    const group = await getItem(chatId) ?? {};
-    newChatMembers.forEach(member =>{
-        if(!group[member.first_name]){
+async function addMembers(chatId, message) {
+    const newChatMembers = message.new_chat_members;
+    const group = await getItem(Math.abs(chatId).toString()) ??
+        (message.from.is_bot === true ?
+            {} : { [message.from.first_name]: { id: message.from.id, spent: 0, owes: {} } }
+        );
+
+    newChatMembers.forEach(member => {
+        if (!group[member.first_name]) {
             group[member.first_name] = {
                 id: member.id,
                 spent: 0,
@@ -98,40 +49,55 @@ async function addMembers(chatId, newChatMembers){
             id: Math.abs(chatId).toString(),
         }
     }).promise();
-    
+
 }
 
-exports.handler = async (event, context, callback) => {
+exports.handler = async (event) => {
 
     const body = JSON.parse(event.body);
     console.log(JSON.stringify(body, null, 2));
 
-    if(body.message?.new_chat_members?.length) {
-        await addMembers(body.message.chat.id, body.message.new_chat_members);
+    if (body.message?.new_chat_members?.length) {
+        await addMembers(body.message.chat.id, body.message);
+        return {
+            statusCode: 200,
+            body: JSON.stringify('OK')
+        }
+    }
+
+    if (!body.message?.text) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify('Nothing to do')
+        }
     }
 
     const existingPeopleRecords = await getItem(Math.abs(body.message.chatId).toString());
 
-    const { peopleRecords, reply, gif } = processMessage(body.message, existingPeopleRecords);
+    const processedMessage = processMessage(body.message, existingPeopleRecords);
 
-    update(Math.abs(body.message.chatId).toString(), peopleRecords);
+    if(processedMessage?.peopleRecords){
+        await update(Math.abs(body.message.chatId).toString(), processedMessage.peopleRecords);
+    }
 
     try {
-        await axios.get(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage?chat_id=${body.message.chat.id}&text=${encodeURI(reply)}`)
-        if(gif) {
-            const gif = await axios.get(`https://g.tenor.com/v1/random?key=${process.env.TENOR_KEY}&q=${encodeURI(gif)}&limit=1`);
+        if (processedMessage.reply){
+            await axios.get(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage?chat_id=${body.message.chat.id}&text=${encodeURI(processedMessage.reply)}`)
+        }
+        if (processedMessage.gif) {
+            const gif = await axios.get(`https://g.tenor.com/v1/random?key=${process.env.TENOR_KEY}&q=${encodeURI(processedMessage.gif)}&limit=1`);
             await axios.get(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendAnimation?chat_id=${body.message.chat.id}&animation=${encodeURI(gif.data.results[0].media[0].gif.url)}`)
         }
     } catch (e) {
         console.error('Error sending message', e);
         await axios.get(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage?chat_id=${body.message.chat.id}&text=${encodeURI('Problems with the server !')}`)
+        // Optional: Throw error so Telegram webhook will retry
     }
 
-    const response = {
+    return {
         statusCode: 200,
         body: JSON.stringify('OK')
     };
-    callback(null, response);
 
 };
 
