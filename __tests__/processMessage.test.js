3a135e62724495ad0b25829393b40cfe1b585b62
Arpan KC <nipeshkc7@gmail.com>
2022-01-30 10:09:59 +1100

    updated tests

--- __tests__/processMessage.test.js
+++ __tests__/processMessage.test.js
@@ -1,10 +1,11 @@
 const { processMessage } = require('../Jesus/processMessage');
 
 let people;
+let peopleOwed;
 
 describe('Testing message processing', () => {
 
-    beforeEach(()=>{
+    beforeEach(() => {
         people = {
             'John': {
                 'spent': 0,
@@ -27,36 +28,84 @@ describe('Testing message processing', () => {
                     'Mary': 0,
                 }
             },
-        }; 
+        };
+
+        peopleOwed = {
+            'John': {
+                'spent': 100,
+                'owes': {
+                    'Mary': 200,
+                    'Bob': 100,
+                }
+            },
+            'Mary': {
+                'spent': 100,
+                'owes': {
+                    'John': 40,
+                    'Bob': 30,
+                }
+            },
+            'Bob': {
+                'spent': 90,
+                'owes': {
+                    'John': 100,
+                    'Mary': 80,
+                }
+            },
+        };
     })
 
     test.each([['John', 300, 100], ['Mary', 600, 200], ['Bob', 100, 33.33]])(
-        "should divide bills equally for %s",(user, spent, divided) => {
-               const result = processMessage({
-                   from: {
-                       first_name: user
-                   },
-                   text: `jesus i spent ${spent} dollars on food`
-               }, people);
+        "should divide bills equally for %s", (user, spent, divided) => {
+            const result = processMessage({
+                from: {
+                    first_name: user
+                },
+                text: `jesus i spent ${spent} dollars on food`
+            }, people);
 
-               Object.keys(result.peopleRecord).forEach(entry => {
-                   if(entry !== user){
-                       expect(result.peopleRecord[entry].owes[user]).toEqual(divided);
-                   }
-               })
-            });
+            Object.keys(result.peopleRecord).forEach(entry => {
+                if (entry !== user) {
+                    expect(result.peopleRecord[entry].owes[user]).toEqual(divided);
+                }
+            })
+        });
 
-     test.each(['John', 'Mary', 'Bob'])(
-         "should clear bills for %s",(user) => {
-                const result = processMessage({
-                    from:{
-                        first_name: user
-                    },
-                    text: 'jesus i cleared my bills'
-                }, people);
-                expect(result.peopleRecord[user].owes).toEqual({});
-         }
-     )
+    test.each([['John', 300, 100], ['Mary', 600, 200], ['Bob', 100, 33.33]])(
+        "should divide bills equally for %s", (user, spent, divided) => {
+            const result = processMessage({
+                from: {
+                    first_name: user
+                },
+                text: `jesus i spent ${spent} dollars on food`
+            }, peopleOwed);
+
+            expect(result.peopleRecord).toMatchSnapshot();
+        });
+
+    test.each(['John', 'Mary', 'Bob'])(
+        "should clear bills for %s", (user) => {
+            const result = processMessage({
+                from: {
+                    first_name: user
+                },
+                text: 'jesus i cleared my bills'
+            }, people);
+            expect(result.peopleRecord[user].owes).toEqual({});
+        }
+    )
+
+    test.each(['John', 'Mary', 'Bob'])(
+        "should return correct reply for %s", (user) => {
+            const result = processMessage({
+                from: {
+                    first_name: user
+                },
+                text: 'jesus how much do i owe?'
+            }, peopleOwed);
+            expect(result.reply).toEqual();
+        }
+    )
 
     it('should throw error if no user', () => {
         expect(() => {
